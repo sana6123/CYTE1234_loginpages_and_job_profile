26e1ec8028f38c011b8b366b316b1fff8ba7ff47
R-bbeck <rebeccatechnovation@gmail.com>
2019-04-13 21:35:42 -0600

    Educational websites results page

---
+++ app/src/main/java/com/example/cytelogin/assessmentpage.java
@@ -0,0 +1,49 @@
+package com.example.cytelogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.app.Activity;
+import android.net.Uri;
+
+
+public class assessmentpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.educatwebsitesassessmentresults);
+
+    }
+    public void goto1 (View view) {
+        goToUrl ( "https://www.pluralsight.com/browse/software-development");
+    }
+
+    public void goto2 (View view) {
+        goToUrl ( "https://teamtreehouse.com/");
+    }
+    public void goto3 (View view) {
+        goToUrl ( "https://www.codecademy.com/");
+    }
+    public void goto11 (View view) {
+        goToUrl ( "https://ca.trustpilot.com/review/pluralsight.com");
+    }
+
+    public void goto22 (View view) {
+        goToUrl ( "https://www.switchup.org/bootcamps/treehouse");
+    }
+    public void goto33 (View view) {
+        goToUrl ( "https://www.switchup.org/bootcamps/codecademy");
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
+
+}
+
+
