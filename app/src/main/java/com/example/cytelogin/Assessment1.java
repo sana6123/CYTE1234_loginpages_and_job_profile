e5d6fad7f0e7038c79423ac4f97c0414d154efb4
helloello1 <flowero68947@gmail.com>
2019-04-13 22:08:23 -0600

    chi

--- app/src/main/java/com/example/cytelogin/Assessment1.java
+++ app/src/main/java/com/example/cytelogin/Assessment1.java
@@ -1,52 +1,42 @@
 package com.example.cytelogin;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
+
 
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Assessment1 extends AppCompatActivity {
 
+    //  SharedPreferences myprefs;
+
+    String ans;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assessmenttest);
 
+        // myprefs = getPreferences(MODE_PRIVATE);
+
         createRadioButtons();
-        setupSubmitAssessmentButton();
+        onSubmit();
+
     }
 
-    private void setupSubmitAssessmentButton(){
-        Button submit_assessment = findViewById(R.id.submit_assess);
-        submit_assessment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RadioGroup group_assessment = findViewById(R.id.radiogroup_assessment);
-                int idSelected = group_assessment.getCheckedRadioButtonId();
-
-
-                RadioButton ans1 = findViewById(idSelected);
-                String show = ans1.getText().toString();
-
-                Toast.makeText(Assessment1.this, "Chose " + show, Toast.LENGTH_SHORT).show();
-
-
-            }
-        });
-    }
 
     private void createRadioButtons (){
         RadioGroup group_assessment = findViewById(R.id.radiogroup_assessment);
 
-        //get items from assessment_array
-        String [] answerChoices = getResources().getStringArray(R.array.assessment_array);
+        //get items from answer_array1
+        String [] answerChoices = getResources().getStringArray(R.array.answer_array1);
 
         ArrayList<String> arrayText = new ArrayList<>();
         Collections.addAll(arrayText, answerChoices);
@@ -56,7 +46,7 @@ public class Assessment1 extends AppCompatActivity {
 
 
         for (int x=0; x<childCount; x++) {
-            //set text of radiobuttons to text in assessment_array
+            //set text of radiobuttons to text in answer_array1
             RadioButton ans1 = (RadioButton) group_assessment.getChildAt(x);
             ans1.setText(arrayText.get(x));
 
@@ -64,22 +54,78 @@ public class Assessment1 extends AppCompatActivity {
             //TODO: NEED THIS??
             final String ansChoice = answerChoices[x];
 
-             //TODO: Set on-click callbacks
-             ans1.setOnClickListener(new View.OnClickListener() {
-                 @Override
-                 public void onClick(View v) {
-                     Toast.makeText(Assessment1.this, "Clicked" + ansChoice, Toast.LENGTH_SHORT).show();
+            //TODO: Set on-click callbacks
+            ans1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(Assessment1.this, "Clicked" + ansChoice, Toast.LENGTH_SHORT).show();
 
-                 }
-             });
+                }
+            });
 
-             //TODO: NEED THIS??
+
+            //TODO: NEED THIS??
             //Add to radio group
             //group_assessment.addView(ans1);
         }
 
     }
 
+    private void onSubmit(){
+        Button submit_assessment = findViewById(R.id.submit_assess);
+        submit_assessment.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                RadioGroup group_assessment = findViewById(R.id.radiogroup_assessment);
+
+                //retrieve information of what button selected
+                int idSelected = group_assessment.getCheckedRadioButtonId();
+                RadioButton ans1 = findViewById(idSelected);
+                String userChoice = ans1.getText().toString();
+
+                //retrieve items from array
+                String[] correctans = getResources().getStringArray(R.array.correct_answers);
+                ArrayList<String> arrayText = new ArrayList<>();
+                Collections.addAll(arrayText, correctans);
+
+                //takes ans from array
+                for (int x = 0; x < correctans.length; x++) {
+                    ans = arrayText.get(x);
+
+                    //check answer, compares userChoice with ans
+                    if(userChoice.equals(ans)){
+                        Toast.makeText(Assessment1.this, "Correct!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(Assessment1.this, "Incorrect!", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }
+                /*
+                //start sharedpreferences editor
+                SharedPreferences.Editor editor = myprefs.edit();
+                editor.putString("keychoice",userChoice);
+                editor.commit();
+*/
+
+            }
+        });
+
+    }
+/*
+    public void readPreferences (){
+        readPreferences();
+        RadioGroup group_assessment = findViewById(R.id.radiogroup_assessment);
+        int idSelected = group_assessment.getCheckedRadioButtonId();
+        RadioButton ans1 = findViewById(idSelected);
+        String userChoice = ans1.getText().toString();
+
+        String ch1 = myprefs.getString("keychoice", "");
+        ans1.setText(ch1);
+        //Toast.makeText(Assessment1.this, "Chose " + ans1, Toast.LENGTH_SHORT).show();
+    }*/
 }
 
 
