77c8142e21aa9b3ae5101d0e4b9fd00428c8240f
helloello1 <flowero68947@gmail.com>
2019-04-19 11:30:20 -0600

    assessment

--- app/src/main/java/com/example/cytelogin/assessment_results.java
+++ app/src/main/java/com/example/cytelogin/assessment_results.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -35,6 +36,7 @@ public class assessment_results extends AppCompatActivity {
 
     public void onRun(){
         TextView result = findViewById(R.id.result);
+        Button linksToEdu = findViewById(R.id.links_to_education);
 
         float temp = (float)score/(float)questionNum;
         int finalScore = (int)(temp*(float)100);
@@ -42,7 +44,9 @@ public class assessment_results extends AppCompatActivity {
         result.setText(String.format(Locale.CANADA, "Score: %d%%", finalScore));
 
         if (finalScore<75){
-            
+            linksToEdu.setVisibility(View.VISIBLE);
+        } else {
+            linksToEdu.setVisibility(View.GONE);
         }
 
 
