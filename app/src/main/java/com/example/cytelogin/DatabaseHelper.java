ee80fbe96158e6ae0d6b187384c3d27be4b11d4d
kanykeinurmambetova <kanykei2610@gmail.com>
2019-04-22 18:53:49 -0600

    HashMap - DatabaseHelper - operations (getJobDetails, insertJobdetails, delete ...)

--- app/src/main/java/com/example/cytelogin/DatabaseHelper.java
+++ app/src/main/java/com/example/cytelogin/DatabaseHelper.java
@@ -406,7 +406,45 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
 
 //CHECK EMPLOYER EMAIL AND PASSWORD
-public boolean checkUser(String email, String password) {
+public boolean CheckEr(String email, String password) {
+
+    // array of columns to fetch
+    String[] columns = {
+            KEY_IDD
+    };
+    SQLiteDatabase db = this.getReadableDatabase();
+    // selection criteria
+    String selection = EMPR_2 + " = ?" + " AND " + EMPR_4 + " = ?";
+
+    // selection arguments
+    String[] selectionArgs = {email, password};
+
+    // query user table with conditions
+    /**
+     * Here query function is used to fetch records from user table this function works like we use sql query.
+     * SQL query equivalent to this query function is
+     * SELECT user_id FROM user WHERE user_email = 'jack@androidtutorialshub.com' AND user_password = 'qwerty';
+     */
+    Cursor cursor = db.query(EMPLOYER_NAME, //Table to query
+            columns,                    //columns to return
+            selection,                  //columns for the WHERE clause
+            selectionArgs,              //The values for the WHERE clause
+            null,                       //group the rows
+            null,                       //filter by row groups
+            null);                      //The sort order
+
+    int cursorCount = cursor.getCount();
+
+    cursor.close();
+    db.close();
+    if (cursorCount > 0) {
+        return true;
+    }
+
+    return false;
+}
+//CHECK EMPLOYES EMAIL AND PASSWORD
+public boolean CheckEe(String email, String password) {
 
     // array of columns to fetch
     String[] columns = {
@@ -448,7 +486,7 @@ public boolean checkUser(String email, String password) {
 
 
 
-        ///////////HashMap
+    ///////////HashMap
         // Adding new Job Details
         void insertJobDetails(String TITLE, String INDUSTRY, String CITY){
             //Get the Data Repository in write mode
